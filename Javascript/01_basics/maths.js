d03608d1264d696ae4e8598fa6c115f29ca11645
Rohaz-bhalla <rohazbhalla3@gmail.com>
2026-07-21 21:18:28 +0530

    maths.js

---
+++ Javascript/01_basics/maths.js
@@ -0,0 +1,13 @@
+let x = 10
+// x = -x
+let y = 5
+let z = '4'
+
+let a = y + x
+let b = x/y //quotient
+let c = x%y // remainder
+
+let d = x + y + z
+let e = z - x + y //if a string comes first rest of number will show concatenated, if - comes it converts string to num
+
+console.table([a,b,c,d,e]);
---
+++ Javascript/01_basics/type_conversions.js
@@ -0,0 +1,8 @@
+// const name = "123"
+let name = "123"
+name = Number(name)
+
+let flag = true
+flag = String(flag)
+
+console.table([typeof(name), typeof(flag)])
